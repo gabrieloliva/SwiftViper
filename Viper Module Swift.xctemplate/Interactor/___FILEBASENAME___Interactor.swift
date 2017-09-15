d33430cbb9c8092e76dc46b658271633c524869e
Gabriel Oliva <gfpoliva@gmail.com>
2017-09-15 16:16:44 -0300

    Updating Interactor template. Some new protocols were created and added in a different file.

---
+++ Viper Module Swift.xctemplate/Interactor/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___Interactor: ___VARIABLE_viperModuleName___InteractorProtocol {
+
+    // MARK: - PROPERTIES
+    var output: ___VARIABLE_viperModuleName___PresenterOutputProtocol!
+    
+}
---
+++ Viper Module Swift.xctemplate/Interactor/___FILEBASENAME___InteractorContracts.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+
+/// This protocol is responsible for output from interactor to presenter
+protocol ___VARIABLE_viperModuleName___InteractorProtocol {
+    
+    /// ___FILEBASENAMEASIDENTIFIER___ presenter output
+    var output: ___VARIABLE_viperModuleName___PresenterOutputProtocol! { get set }
+    
+    // Write ___VARIABLE_viperModuleName___ outputs protocols down below...
+
+}

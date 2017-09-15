8fc8c90c72af3ace050904ae19883237a4ab97b5
Gabriel Oliva <gfpoliva@gmail.com>
2017-09-15 15:38:00 -0300

    View controller template update. Now contracts are in a different file

---
+++ Viper Module Swift.xctemplate/View/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___: UIViewController, ___FILEBASENAMEASIDENTIFIER___Protocol {
+	
+    // MARK: - PROPERTIES
+	var input: ___VARIABLE_viperModuleName___PresenterInputProtocol!
+
+    // MARK: - LIFE CYCLE
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Viper Module Swift.xctemplate/View/___FILEBASENAME___ViewControllerContracts.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+/// This protocol is responsible for **Presenter -> ViewController** communication
+class ___VARIABLE_viperModuleName___ViewControllerProtocol {
+    
+    /// ___VARIABLE_viperModuleName___ presenter input
+    var input: ___VARIABLE_viperModuleName___PresenterInputProtocol! { get set }
+
+    // Write ___VARIABLE_viperModuleName___ protocols down below...
+}
